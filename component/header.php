7fa9ac15c815cf9b964b8a8a32e13f7b00e659d8
DS Media Group Developement Team <opensource@dsgroupmedia.com>
2020-05-14 12:44:43 +0700

    Update header.php

--- component/header.php
+++ component/header.php
@@ -10,7 +10,7 @@
   <!-- Meta Tag SEO -->
   <meta name="description" content="Whois Service Data The Domain & IP 100 % Free & No Ads" >
     <meta name="keywords" lang="en" content="whois, mexio.us, script whois, domain, ip" >
-      <title>Whois Data Domain & IP  <?php echo "$domain"; ?></title>
+      <title>Whois Data Domain & IP </title>
   <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
       <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
